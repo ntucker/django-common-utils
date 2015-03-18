1e392f397712de2703189e23a791e6aeb4bec412
Nathaniel Tucker <me@ntucker.me>
2015-03-18 11:01:35 +0000

    Add underslug filter

--- utils/templatetags/common.py
+++ utils/templatetags/common.py
@@ -1,6 +1,7 @@
 from __future__ import unicode_literals
 
 from django import template
+from django.template.defaultfilters import stringfilter
 
 register = template.Library()
 
@@ -8,3 +9,8 @@ register = template.Library()
 @register.simple_tag(takes_context=True)
 def absolute_url(context, obj):
     return obj.get_absolute_url(context.get('request', None))
+
+@register.filter
+@stringfilter
+def underslug(string):
+    return string.replace(" ", "_").replace("'", "").lower()
